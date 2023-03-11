928445a02885a0430a8ed3e6a7890576b9eaa07c
Tamas Kovacs <tk@delodi.net>
2023-03-11 21:34:32 +0100

    Smaller fixes on mobiel device

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -11,16 +11,17 @@ class Card extends React.Component {
         <div>
           <h1>Tamás Kovács</h1>
 
-          <Link to="/" className="home-link">
-            &#8962;
-          </Link>
-          <Link to="/privacy.html">Privacy</Link>
-          <Link to="/mobiles.html">Mobiles</Link>
+          <center>
+            <Link to="/" className="home-link">
+              &#8962;
+            </Link>
+            <Link to="/privacy.html">Privacy</Link>
+            <Link to="/mobiles.html">Mobiles</Link>
 
-          <a href="https://www.linkedin.com/in/kovacs-tamas/">Linkedin</a>
-          <a href="https://github.com/kovacs-tamas">Github</a>
-          <a href="https://twitter.com/lengfelderr">Twitter</a>
-
+            <a href="https://www.linkedin.com/in/kovacs-tamas/">Linkedin</a>
+            <a href="https://github.com/kovacs-tamas">Github</a>
+            <a href="https://twitter.com/lengfelderr">Twitter</a>
+          </center>
           <img src={tamas} className="tamas shadowed" alt="Tamas Kovacs" />
         </div>
       </div>
