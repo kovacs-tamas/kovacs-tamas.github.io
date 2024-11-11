ef7efe6e5ca6485abfa4eee5e073e99235e7dfef
Tamas Kovacs <kovacs.tamas@me.com>
2024-11-11 08:59:16 +0100

    Smaller refactoring

--- src/containers/Alma/Alma.js
+++ src/containers/Alma/Alma.js
@@ -9,9 +9,14 @@ import linkedin from '../../images/linkedin.png';
 import './Alma.scss';
 
 class Alma extends React.Component {
-  play(url) {
+  files = [
+    'https://babelium.blob.core.windows.net/staging/response/54/69ca56b2-e648-4684-b56e-e42588fb3ffd.webm?1730811387881',
+    'https://babelium.blob.core.windows.net/production/response/51539/d93af598-95f3-40b6-8bc3-e892aff6d22d.webm?1730141792324',
+    'https://babelium.blob.core.windows.net/staging/response/55/014feb9e-cc49-495f-9324-61a42c8b2ca0.webm?1730812371608',
+  ];
+  play(id) {
     var audio = document.createElement('audio');
-    audio.src = url;
+    audio.src = this.files[id];
     audio.play();
     audio.onloadeddata = function (data) {
       console.log('audio loaded', this, data);
@@ -21,34 +26,24 @@ class Alma extends React.Component {
   render() {
     return (
       <div className="mobiles">
+        {this.files.map((audioURL, i) => (
+          <div key={`audio_${i}`}>
+            <video
+              id={'video' + i}
+              controls
+              playsInline
+              style={{
+                width: 200,
+                display: 'block',
+              }}
+              src={audioURL}
+            />
+          </div>
+        ))}
         <div>
-          <button
-            onClick={() =>
-              this.play(
-                'https://babelium.blob.core.windows.net/staging/response/54/69ca56b2-e648-4684-b56e-e42588fb3ffd.webm?1730811387881'
-              )
-            }
-          >
-            Firefox
-          </button>
-          <button
-            onClick={() =>
-              this.play(
-                'https://babelium.blob.core.windows.net/production/response/51539/d93af598-95f3-40b6-8bc3-e892aff6d22d.webm?1730141792324'
-              )
-            }
-          >
-            Firefox / Constantin
-          </button>
-          <button
-            onClick={() =>
-              this.play(
-                'https://babelium.blob.core.windows.net/staging/response/55/014feb9e-cc49-495f-9324-61a42c8b2ca0.webm?1730812371608'
-              )
-            }
-          >
-            Chrome
-          </button>
+          <button onClick={() => this.play(0)}>Firefox</button>
+          <button onClick={() => this.play(1)}>Firefox / Constantin</button>
+          <button onClick={() => this.play(2)}>Chrome</button>
           &nbsp;
         </div>
       </div>
